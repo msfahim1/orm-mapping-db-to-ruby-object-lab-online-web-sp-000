be4c0ea66c05f2d325a5dd5cca518d080a7a130f
Michael Fahim <michael_fahim@yahoo.com>
2020-03-28 17:03:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,11 +59,6 @@ def self.new_from_db(row)
   student = Student.new(row[1], row[2], row[0])
   return student
 end
-def update
-  sql = <<-SQL
-        UPDATE students SET name=?, grade=? WHERE id=?
-        SQL
-  DB[:conn].execute(sql, @name, @grade, @id)
-end
+
  
 end
