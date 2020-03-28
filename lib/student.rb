1fb7330eabf0ffdca2d7413579d06e38b7d72cb7
Michael Fahim <michael_fahim@yahoo.com>
2020-03-28 16:45:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,5 +40,11 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
-  
+  def self.new_from_db(row)
+  new_song = self.new  # self.new is the same as running Song.new
+  new_song.id = row[0]
+  new_song.name =  row[1]
+  new_song.length = row[2]
+  new_song  # return the newly created instance
+end
 end
